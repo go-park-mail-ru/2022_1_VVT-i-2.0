ba671650819d6b72393769fa5b8e3d68c790204e
Natali-Skv <nat-s.skv@mail.ru>
2022-04-16 10:51:56 +0300

    minifix

--- internal/app/user/delivery/http/handler.go
+++ internal/app/user/delivery/http/handler.go
@@ -189,7 +189,7 @@ func (h UserHandler) SendCode(ctx echo.Context) error {
 func (h UserHandler) GetUser(ctx echo.Context) error {
 	user := middleware.GetUserFromCtx(ctx)
 	if user == nil {
-		return ctx.JSON(http.StatusOK, models.UserDataResp{})
+		return ctx.JSON(http.StatusOK, ``)
 	}
 
 	logger := middleware.GetLoggerFromCtx(ctx)
