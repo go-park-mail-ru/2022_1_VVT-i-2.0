0e5cad41fd1b57c7182ceaa0eda9dfce6355a4ac
yut_fut <seregey.golubev@mail.ru>
2022-05-25 02:09:14 +0300

    new test

--- internal/app/dishes/usecase/usecase_test.go
+++ internal/app/dishes/usecase/usecase_test.go
@@ -17,16 +17,16 @@ func TestDishesUcase_RestaurantDishes(t *testing.T) {
 	assert.NoError(t, err)
 
 	expectResp := &models.GetRestaurantDishesCategoriesUcaseResp{
-		Id:                   1,
-		Name:                 "Name",
-		ImagePath:            "ImagePath",
-		Slug:                 "Slug",
-		MinPrice:             1,
-		AggRating:            9,
-		ReviewCount:          2,
-		UpMinutesToDelivery:   3,
-		DownMinutesToDelivery: 2,
-		Dishes:               []models.DishCategoriesUsecase{{Id: 1, Category: 1, Name: "Name", Description: "Description", Price: 10, Weight: 100, Calories: 200, RestaurantId: 1, ImagePath: "DishImagePath"}},
+		Id:                   	1,
+		Name:                 	"Name",
+		ImagePath:            	"ImagePath",
+		Slug:                 	"Slug",
+		MinPrice:             	1,
+		AggRating:            	9,
+		ReviewCount:         	2,
+		UpMinutesToDelivery:   	3,
+		DownMinutesToDelivery:	2,
+		Dishes:               	[]models.DishCategoriesUsecase{{Id: 1, Category: 1, Name: "Name", Description: "Description", Price: 10, Weight: 100, Calories: 200, RestaurantId: 1, ImagePath: "DishImagePath"}},
 		Categories:				[]models.CategoriesDishesUcaseResp{{Categories: "1", Dishes: []int{1}}},
 	}
 
@@ -36,28 +36,14 @@ func TestDishesUcase_RestaurantDishes(t *testing.T) {
 	}
 }
 
-func TesDishesUcase_GetRestaurantDishes_EmptySlug(t *testing.T) {
-	mockRestaurantsRepo := new(mock.DishesRepo)
+func TestDishesUcase_RestaurantDishes_EmptySlug(t *testing.T) {
+	mockRestaurantsRepo := new(mock.DishesRepoErr)
 	ucase := NewDishesUcase(mockRestaurantsRepo)
 
-	dishData, err := ucase.GetRestaurantDishes(models.GetRestaurantDishesUcaseReq{Slug: "slug"})
-	assert.NoError(t, err)
+	dishData, _ := ucase.GetRestaurantDishes(models.GetRestaurantDishesUcaseReq{Slug: "slug"})
 
-	expectResp := &models.GetRestaurantDishesUcaseResp{
-		Id:                   1,
-		Name:                 "Name",
-		ImagePath:            "ImagePath",
-		Slug:                 "Slug",
-		MinPrice:             1,
-		AggRating:            9,
-		ReviewCount:          2,
-		UpMinutsToDelivery:   3,
-		DownMinutsToDelivery: 2,
-		Dishes:               []models.DishUcase{{Id: 1, Name: "Name", Description: "Description", Price: 10, Weight: 100, Calories: 200, RestaurantId: 1, ImagePath: "DishImagePath"}},
-	}
-
-	if !reflect.DeepEqual(dishData, expectResp) {
-		t.Errorf("results not match, want %v, have %v", dishData.Dishes[0], expectResp)
+	if reflect.DeepEqual(dishData, nil) {
+		t.Errorf("results not match, want %v, have %v", dishData, nil)
 		return
 	}
 }
