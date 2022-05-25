a767580f8ee406c52b104cf49b3a3ac69a0f6089
yut_fut <seregey.golubev@mail.ru>
2022-05-25 03:55:02 +0300

    added test for parser

--- internal/app/tools/addrParser/parser_test.go
+++ internal/app/tools/addrParser/parser_test.go
@@ -5,106 +5,75 @@ import (
 	"testing"
 )
 
-func TestConcatAddr(t *testing.T) {
-	type args struct {
-		city   string
-		street string
-		house  string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ConcatAddr(tt.args.city, tt.args.street, tt.args.house); got != tt.want {
-				t.Errorf("ConcatAddr() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestConcatAddrToComplete(t *testing.T) {
-	type args struct {
-		city   string
-		street string
-		house  string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ConcatAddrToComplete(tt.args.city, tt.args.street, tt.args.house); got != tt.want {
-				t.Errorf("ConcatAddrToComplete() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestGetCity(t *testing.T) {
-	type args struct {
-		city string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetCity(tt.args.city); got != tt.want {
-				t.Errorf("GetCity() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGetHouse(t *testing.T) {
-	type args struct {
-		house string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetHouse(tt.args.house); got != tt.want {
-				t.Errorf("GetHouse() = %v, want %v", got, tt.want)
-			}
-		})
+	response := GetCity("")
+	if !reflect.DeepEqual(response, "") {
+		t.Errorf("results not match, want %v, have %v", response, "")
+		return
 	}
 }
 
 func TestGetStreet(t *testing.T) {
-	type args struct {
-		streetStr string
+	response := GetStreet("")
+	expect := &StreetT{}
+	if !reflect.DeepEqual(response, expect) {
+		t.Errorf("results not match, want %v, have %v", response, expect)
+		return
 	}
-	tests := []struct {
-		name string
-		args args
-		want *StreetT
-	}{
-		// TODO: Add test cases.
+}
+
+func TestGetStreetErr(t *testing.T) {
+	response := GetStreet("")
+	if reflect.DeepEqual(response, "") {
+		t.Errorf("results not match, want %v, have %v", response, "")
+		return
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetStreet(tt.args.streetStr); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetStreet() = %v, want %v", got, tt.want)
-			}
-		})
+}
+
+func TestGetHouse(t *testing.T) {
+	response := GetHouse("")
+	if !reflect.DeepEqual(response, "") {
+		t.Errorf("results not match, want %v, have %v", response, "")
+		return
+	}
+}
+
+func TestConcatAddr(t *testing.T) {
+	response := ConcatAddr("1", "2", "3")
+	if !reflect.DeepEqual(response, "1, 2, 3") {
+		t.Errorf("results not match, want %v, have %v", response, "1, 2, 3")
+		return
+	}
+}
+
+func TestConcatAddrErr(t *testing.T) {
+	response := ConcatAddr("1", "2", "3")
+	if reflect.DeepEqual(response, "1,2,3") {
+		t.Errorf("results not match, want %v, have %v", response, "1,2,3")
+		return
+	}
+}
+
+func TestConcatAddrToComplete(t *testing.T) {
+	response := ConcatAddrToComplete("1", "2", "3")
+	if !reflect.DeepEqual(response, "1, 2, 3") {
+		t.Errorf("results not match, want %v, have %v", response, "1, 2, 3")
+		return
+	}
+}
+
+func TestConcatAddrToComplete_WithOutHouse(t *testing.T) {
+	response := ConcatAddrToComplete("1", "2", "")
+	if !reflect.DeepEqual(response, "1, 2") {
+		t.Errorf("results not match, want %v, have %v", response, "1, 2")
+		return
+	}
+}
+
+func TestConcatAddrToComplete_WithOutHouseErr(t *testing.T) {
+	response := ConcatAddrToComplete("1", "2", "")
+	if reflect.DeepEqual(response, "1, 2, 3") {
+		t.Errorf("results not match, want %v, have %v", response, "1, 2, 3")
+		return
 	}
 }
